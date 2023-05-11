3eb5a2f5d65cb370e21b94ce33c36375e708a805
kody.low <kodylow7@gmail.com>
2023-05-11 23:11:04 +0000

    llms done

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from langchain.llms import OpenAI
 import os
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.chat_models import ChatOpenAI
 
-my_openai_api_key = os.environ['OPENAI_API_KEY']
+# my_openai_api_key = os.environ['OPENAI_API_KEY']
 
-print(my_openai_api_key)
+# llm = OpenAI(model_name="text-curie-001")
+
+# result = llm("Write a poem.")
+
+# print(result)
+
+# num_tokens = llm.get_num_tokens("I am writing Python code!")
+# print("Num tokens:", num_tokens)
+
+# llm = OpenAI(streaming=True, callbacks=[StreamingStdOutCallbackHandler()], temperature=1)
+# print(llm("Tell me a joke about dogs."))
+
+chat = ChatOpenAI(temperature=0.5)
